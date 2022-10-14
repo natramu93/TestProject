3a4e70d71613d3aa6c43a27076be4108942e47de
Natarajan Ramanathan <natarajan@saanshealth.com>
2022-10-14 14:50:27 +0530

    Updated JSON

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/resources",glue="steps")
+@CucumberOptions(features="src/test/resources",glue="steps",plugin= {"json:Reports.json"})
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
